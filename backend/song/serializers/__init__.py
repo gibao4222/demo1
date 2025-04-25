44bab6f8dc994850302d2668c3c865adc1f266bc
Gia Bao <voquang17@gmail.com>
2025-04-25 22:41:56 +0700

    fix code serializer song

--- backend/song/serializers/__init__.py
+++ backend/song/serializers/__init__.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from ..models import Song
-from singer.models import Singer
+# from singer.models import Singer
 
 class SingerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,19 +9,20 @@ class SingerSerializer(serializers.ModelSerializer):
 
 class SongSerializer(serializers.ModelSerializer):
 
-    singers = serializers.SerializerMethodField()
+    # singers = serializers.SerializerMethodField()
     # Thêm trường album để lấy thông tin album từ mối quan hệ song_albums
     album = serializers.SerializerMethodField()
     
     class Meta:
         model = Song
-        fields = ['id', 'name', 'image', 'popularity', 'release_date', 'url_lyric', 'url_song', 'url_video', 'singers']
+        # fields = ['id', 'name', 'image', 'popularity', 'release_date', 'url_lyric', 'url_song', 'url_video', 'singers']
+        fields ='__all__'
         
-    def get_singers(self, obj):
-        # Lấy danh sách nghệ sĩ thông qua bảng trung gian SingerSong
-        singer_songs = obj.song_singers.all()  # Sử dụng related_name='song_singers'
-        singers = [singer_song.id_singer for singer_song in singer_songs]
-        return SingerSerializer(singers, many=True).data
+    # def get_singers(self, obj):
+    #     # Lấy danh sách nghệ sĩ thông qua bảng trung gian SingerSong
+    #     singer_songs = obj.song_singers.all()  # Sử dụng related_name='song_singers'
+    #     singers = [singer_song.id_singer for singer_song in singer_songs]
+    #     return SingerSerializer(singers, many=True).data
     
     def get_album(self, obj):
         try:
