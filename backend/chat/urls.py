61b0842b304fed510e04dbcee648a4e96e7a2ec8
Gia Bao <voquang17@gmail.com>
2025-05-04 03:20:32 +0700

    commit

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ChatPermissionView, MessageView, MessageListView
-from .views_chatbot_v2 import chatbot
+from .views_chatbot import chatbot
 urlpatterns = [
     path('permission/', ChatPermissionView.as_view(), name='chat-permission'),
     path('message/', MessageView.as_view(), name='send-message'),
